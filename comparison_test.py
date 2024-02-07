04f3ebf74f552f593fda9291d00ede3f74d5a355
LennertDeSmet <lennert.desmet@kuleuven.be>
2024-02-08 00:07:11 +0100

    Added log-space version of addition

--- comparison_test.py
+++ comparison_test.py
@@ -4,3 +4,5 @@ number1 = DiscreteRV([0.1 for _ in range(10)], 0, 9)
 number2 = DiscreteRV([0.1 for _ in range(10)], 5, 15)
 
 print(number1 <= number2)
+print(number1 == number2)
+print(number1 != number2)
--- discrete_rv.py
+++ discrete_rv.py
@@ -8,22 +8,37 @@ EPS = 1e-16
 
 class DiscreteRV:
 
-    def __init__(self, probs: [tf.Tensor, list[float]], domain_min: int, domain_max: int):
+    def __init__(self, probs: [tf.Tensor, list[float]], domain_min: int, domain_max: int, log_space: bool = True):
         self.probs = tf.constant(probs)
         self.domain = IntegerInterval(domain_min, domain_max)
+        self.log_space = log_space
+        # TODO: add separate id for zero entry and propagate through operations. Avoids linear/log time search for zero.
 
     def __add__(self, other: [int, float, 'DiscreteRV']):
         if isinstance(other, DiscreteRV):
-            """ Assuming the domain is connected, we use min and max to find the new domain in linear time """
+            """ Assuming the domain is connected, we update min and max to find the new domain in constant time """
             domain = self.domain + other.domain
             n = domain.max - domain.min + 1
 
-            fft1 = tf.signal.rfft(tf.cast(self.probs, dtype=tf.float64), [n])
-            fft2 = tf.signal.rfft(tf.cast(other.probs, dtype=tf.float64), [n])
-            fft = fft1 * fft2
-            probs = tf.signal.irfft(fft, [n])
-            probs /= tf.reduce_sum(probs)
-            probs = tf.cast(probs, dtype=tf.float32)
+            if self.log_space:
+                log_p1 = tf.math.log(self.probs + EPS)
+                log_p2 = tf.math.log(other.probs + EPS)
+
+                log_fft1 = tf.signal.rfft(log_p1, [n])
+                log_fft2 = tf.signal.rfft(log_p2, [n])
+                log_fft = log_fft1 * log_fft2
+                log_probs = tf.signal.irfft(log_fft, [n])
+
+                log_sum_exp = tf.reduce_logsumexp(log_probs, -1)
+                log_probs -= log_sum_exp
+                probs = tf.exp(log_probs)
+            else:
+                fft1 = tf.signal.rfft(tf.cast(self.probs, dtype=tf.float64), [n])
+                fft2 = tf.signal.rfft(tf.cast(other.probs, dtype=tf.float64), [n])
+                fft = fft1 * fft2
+                probs = tf.signal.irfft(fft, [n])
+                probs /= tf.reduce_sum(probs)
+                probs = tf.cast(probs, dtype=tf.float32)
 
             return DiscreteRV(probs, domain.min, domain.max)
         elif isinstance(other, (int, float)):
@@ -95,6 +110,23 @@ class DiscreteRV:
     def __gt__(self, other):
         if isinstance(other, (int, float, DiscreteRV)):
             return 1. - (self <= other)
+        else:
+            raise NotImplementedError("You can only compare a DiscreteRV to another DiscreteRV")
+
+    def __eq__(self, other):
+        if isinstance(other, (int, float, DiscreteRV)):
+            sub = self - other
+            zero_id = self.find_zero(sub.domain)
+            if zero_id is None:
+                return 1.
+            else:
+                return sub.probs[..., zero_id]
+        else:
+            raise NotImplementedError("You can only compare a DiscreteRV to another DiscreteRV")
+
+    def __ne__(self, other):
+        if isinstance(other, (int, float, DiscreteRV)):
+            return 1. - (self == other)
         else:
             raise NotImplementedError("You can only compare a DiscreteRV to another DiscreteRV")
 
