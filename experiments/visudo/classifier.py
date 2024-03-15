0d9f38cff408f69ba25df028dba0f5a6e6537e0f
LennertDeSmet <lennert.desmet@kuleuven.be>
2024-03-16 00:21:19 +0100

    ViSudo ready + redo MNIST dataloading

--- experiments/visudo/classifier.py
+++ experiments/visudo/classifier.py
@@ -44,7 +44,7 @@ class SudokuSolver(tf.keras.Model):
         representation = np.array(representation, dtype=object)
         return representation
 
-    def distinct_numpy_row_elements(self, inputs):
+    def distinct_row_elements(self, inputs):
         x = E.reduce(inputs, "row column binaries -> (row binaries)", "sum")
         row_constraints = [0] * len(x)
         for i, _ in enumerate(x):
@@ -52,24 +52,7 @@ class SudokuSolver(tf.keras.Model):
         row_constraints = tf.stack(row_constraints, axis=1)
         return row_constraints
 
-    def distinct_row_elements(self, inputs):
-        row_constraints = []
-        for i, row in enumerate(inputs):
-            row_constraints.append([0] * self.grid_size)
-            for column in row:
-                for k, binary in enumerate(column):
-                    row_constraints[i][k] += binary
-        for i, row in enumerate(row_constraints):
-            for k, binary in enumerate(row):
-                row_constraints[i][k] = log_expectation(row_constraints[i][k] == 1)
-            row_constraints[i] = tf.stack(row_constraints[i], axis=1)
-        row_constraints = tf.stack(row_constraints, axis=1)
-        row_constraints = E.rearrange(
-            row_constraints, "b row binaries -> b (row binaries)"
-        )
-        return row_constraints
-
-    def distinct_numpy_column_elements(self, inputs):
+    def distinct_column_elements(self, inputs):
         x = E.reduce(inputs, "row column binaries -> (column binaries)", "sum")
         column_constraints = [0] * len(x)
         for j, _ in enumerate(x):
@@ -77,26 +60,7 @@ class SudokuSolver(tf.keras.Model):
         column_constraints = tf.stack(column_constraints, axis=1)
         return column_constraints
 
-    def distinct_column_elements(self, inputs):
-        column_constraints = []
-        for j in range(self.grid_size):
-            column_constraints.append([0] * self.grid_size)
-            for i in range(self.grid_size):
-                for k in range(self.grid_size):
-                    column_constraints[j][k] += inputs[i][j][k]
-        for j, column in enumerate(column_constraints):
-            for k, binary in enumerate(column):
-                column_constraints[j][k] = log_expectation(
-                    column_constraints[j][k] == 1
-                )
-            column_constraints[j] = tf.stack(column_constraints[j], axis=1)
-        column_constraints = tf.stack(column_constraints, axis=1)
-        column_constraints = E.rearrange(
-            column_constraints, "b column binaries -> b (column binaries)"
-        )
-        return column_constraints
-
-    def distinct_numpy_box_elements(self, inputs):
+    def distinct_box_elements(self, inputs):
         row = inputs.shape[0]
         column = inputs.shape[1]
         binaries = inputs.shape[2]
@@ -122,43 +86,15 @@ class SudokuSolver(tf.keras.Model):
         box_constraints = tf.stack(box_constraints, axis=1)
         return box_constraints
 
-    def distinct_box_elements(self, inputs):
-        box_constraints = []
-        for i in range(self.grid_size // 3):
-            box_constraints.append([])
-            for j in range(self.grid_size // 3):
-                box_constraints[i].append([0] * self.grid_size)
-                for box_row in range(self.grid_size // 3):
-                    for box_column in range(self.grid_size // 3):
-                        for k in range(self.grid_size):
-                            box_constraints[i][j][k] += inputs[i * 3 + box_row][
-                                j * 3 + box_column
-                            ][k]
-
-        for i, box_row in enumerate(box_constraints):
-            for j, box_column in enumerate(box_row):
-                for k, binary in enumerate(box_column):
-                    box_constraints[i][j][k] = log_expectation(
-                        box_constraints[i][j][k] == 1
-                    )
-                box_constraints[i][j] = tf.stack(box_constraints[i][j], axis=1)
-            box_constraints[i] = tf.stack(box_constraints[i], axis=1)
-        box_constraints = tf.stack(box_constraints, axis=1)
-        box_constraints = E.rearrange(
-            box_constraints,
-            "b box_row box_column binaries -> b (box_row box_column binaries)",
-        )
-        return box_constraints
-
     def call(self, inputs, training=None, mask=None):
         grid_size = inputs.shape[-1]
 
         x = self.binary_numpy_representation(inputs, grid_size)
-        row_constraint = self.distinct_numpy_row_elements(x)
-        column_constraint = self.distinct_numpy_column_elements(x)
+        row_constraint = self.distinct_row_elements(x)
+        column_constraint = self.distinct_column_elements(x)
 
         if grid_size == 9:
-            box_constraint = self.distinct_numpy_box_elements(x)
+            box_constraint = self.distinct_box_elements(x)
             return tf.concat(
                 [row_constraint, column_constraint, box_constraint], axis=-1
             )
